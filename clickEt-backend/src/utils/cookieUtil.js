76456cda81f16c6a6e55a3ae57181b669070e6ab
manishaa8981 <manishaa8981@gmail.com>
2025-07-23 13:08:38 +0545

    cookies

--- clickEt-backend/src/utils/cookieUtil.js
+++ clickEt-backend/src/utils/cookieUtil.js
@@ -1,16 +1,13 @@
 // src/utils/cookieUtil.js
 import crypto from "crypto";
-export const setTokenCookie = (response, cookie_name, tokenValue) => {
-  const cookieOptions = {
+export function setTokenCookie(res, name, token) {
+  res.cookie(name, token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
-    maxAge: process.env.JWT_REFRESH_EXPIRY * 24 * 60 * 60 * 1000,
-    path: "/",
-  };
-
-  response.cookie(cookie_name, tokenValue, cookieOptions);
-};
+    sameSite: "Strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+}
 
 export const hashCrypto = (refreshToken) =>
   crypto.createHash("sha256").update(refreshToken).digest("hex");
