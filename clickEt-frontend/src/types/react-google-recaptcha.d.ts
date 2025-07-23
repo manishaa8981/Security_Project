f1c8ab15c53661c97840e9b4fba199a50af980ea
manishaa8981 <manishaa8981@gmail.com>
2025-07-23 13:09:32 +0545

    google-reCAPTCHA

---
+++ clickEt-frontend/src/types/react-google-recaptcha.d.ts
@@ -0,0 +1,16 @@
+declare module 'react-google-recaptcha' {
+  import * as React from 'react';
+
+  export interface ReCAPTCHAProps {
+    sitekey: string;
+    onChange?: (token: string | null) => void;
+    onExpired?: () => void;
+    onErrored?: () => void;
+    theme?: 'light' | 'dark';
+    size?: 'compact' | 'normal' | 'invisible';
+    tabindex?: number;
+    badge?: 'bottomright' | 'bottomleft' | 'inline';
+  }
+
+  export default class ReCAPTCHA extends React.Component<ReCAPTCHAProps> {}
+}
